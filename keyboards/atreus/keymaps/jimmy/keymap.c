dda473de0070b3c3efbc11075372acedf9e98dab
Jimmy Ngu <jimmynguyc@gmail.com>
2020-04-07 14:38:11 +0800

    Comment

--- keyboards/atreus/keymaps/jimmy/keymap.c
+++ keyboards/atreus/keymaps/jimmy/keymap.c
@@ -12,6 +12,12 @@
 #define _LW 2
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  /*  Qwerty
+   * q    w     e      r    t           ||          y       u   i   o   p
+   * a    s     d      f    g           ||          h       j   k   l   ;
+   * z    x     c      v    b           ||          n       m   ,   .   /
+   * esc  tab   ctrl   alt  bksp  super ||  shift   space   L1  -   '   enter
+   */
   [_QW] = LAYOUT( /* Qwerty */
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
